6b233fc5494f491d830aa6f7c2140ff625446896
KaranSinghG <karansinghg@ymail.com>
2026-02-17 00:00:38 +0530

    Initialize backend structure with Maven setup and Spring Boot application

---
+++ backend/src/test/java/com/fitmetrics/analystics/analystics_service/AnalysticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmetrics.analystics.analystics_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalysticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
