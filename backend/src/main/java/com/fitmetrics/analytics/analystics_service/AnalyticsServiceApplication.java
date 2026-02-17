14b16ac38026d9f999e197162b25c5b754f2577c
KaranSinghG <karansinghg@ymail.com>
2026-02-17 18:56:11 +0530

    Fix spelling errors in package names

--- backend/src/main/java/com/fitmetrics/analytics/analystics_service/AnalyticsServiceApplication.java
+++ backend/src/main/java/com/fitmetrics/analytics/analystics_service/AnalyticsServiceApplication.java
@@ -1,13 +1,13 @@
-package com.fitmetrics.analystics.analystics_service;
+package com.fitmetrics.analytics.analystics_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnalysticsServiceApplication {
+public class AnalyticsServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnalysticsServiceApplication.class, args);
+		SpringApplication.run(AnalyticsServiceApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/fitmetrics/analytics/analystics_service/AnalyticsServiceApplicationTests.java
+++ backend/src/test/java/com/fitmetrics/analytics/analystics_service/AnalyticsServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fitmetrics.analystics.analystics_service;
+package com.fitmetrics.analytics.analystics_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AnalysticsServiceApplicationTests {
+class AnalyticsServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
